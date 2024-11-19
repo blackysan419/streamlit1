d2e96bfbfdce8af92e24425b3e4703918dc8bd4c
blackysan419 <103358307+blackysan419@users.noreply.github.com>
2024-11-19 11:18:01 +0900

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-import streamlit as st
+# -*- coding: utf-8 -*-
 
-#タイトル
-st.title("ぶらっきーのゆっくり解説")
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.title("Web App")
+data = {
+    'lat': np.random.randn(100) / 100 + 35.68,
+    'lon': np.random.randn(100) / 100 + 139.75,
+}
+map_data = pd.DataFrame(data)
+# 地図に散布図を描く
+st.map(map_data)
